c25c34f9381174048dce1ed903f5ea2e46af02d9
Tomasz Ducin <tomasz.ducin@gmail.com>
2013-10-26 11:00:53 +0200

    shell test script

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# setting pythonpath to enable python client module loading
+export PYTHONPATH="$PYTHONPATH:`pwd`/client/python"
+nosetests --nocapture client/python
+py_status=$?
+
+cd client/php && phpunit && cd ../..
+php_status=$?
+
+# return error exit status code if any of tests have failed
+status=$(($py_status + $php_status))
+exit $status
