a0767228d392234ec972f2ff679faa1c073604e2
Tomasz Ducin <tomasz.ducin@gmail.com>
2013-10-26 11:09:56 +0200

    improved test script output

--- test.sh
+++ test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+echo -e "\n>>> python client test <<<\n\n"
 # setting pythonpath to enable python client module loading
 export PYTHONPATH="$PYTHONPATH:`pwd`/client/python"
 nosetests --nocapture client/python
 py_status=$?
 
+echo -e "\n\n\n>>> php client test <<<\n"
 cd client/php && phpunit && cd ../..
 php_status=$?
 
